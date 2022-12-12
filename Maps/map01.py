905cabe3c38640218ed35426b51f2123451b1c44
JaysinH13 <90346481+JaysinH13@users.noreply.github.com>
2022-12-12 09:02:01 -0500

    Update map01.py

--- Maps/map01.py
+++ Maps/map01.py
@@ -3,6 +3,11 @@ import pyautogui
 
 
 # time.sleep(5)
+
+
+# Follow pyautogui.moveTo(x,y) with a pyautogui.dragTo(x,y, button='left') that way it will drag the towers 
+
+
 # pyautogui.click(977, 674, duration = 0)
 # pyautogui.click(977, 674, duration = 0)
 # print(pyautogui.size())
