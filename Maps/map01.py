1eda1fc4fbf54f56be15cc3d099bb2520a93d4f8
JaysinH13 <90346481+JaysinH13@users.noreply.github.com>
2022-12-12 11:06:12 -0500

    Mouse Drag Works
    Got mouse drag to work. Credit to Jason Lara for the help

--- Maps/map01.py
+++ Maps/map01.py
@@ -5,13 +5,14 @@ import pyautogui
 # Follow pyautogui.moveTo(x,y) with a pyautogui.dragTo(x,y, button='left') that way it will drag the towers 
 
 
-# pyautogui.mouseInfo()
-time.sleep(5)
+time.sleep(1)
 pyautogui.click(966, 960)
 pyautogui.click(234, 98)
 pyautogui.click(944, 647)
 pyautogui.moveTo(472,963)
-# pyautogui.click(977, 674, duration = 0)
+pyautogui.mouseDown(button='left')
+pyautogui.moveTo(384, 635, 0.15)
+pyautogui.mouseUp(button='left')
 # print(pyautogui.size())
 screenSize = pyautogui.size()
 # print(screenSize[0], screenSize[1])
