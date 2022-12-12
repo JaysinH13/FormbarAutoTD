f92d2ffb7444e9dc8b61fda0f6dad5a9a1d7032d
JaysinH13 <90346481+JaysinH13@users.noreply.github.com>
2022-12-12 11:12:04 -0500

    Update map01.py
    Added Enabled Speedup along with moving into function

--- Maps/map01.py
+++ Maps/map01.py
@@ -4,20 +4,20 @@ import pyautogui
 
 # Follow pyautogui.moveTo(x,y) with a pyautogui.dragTo(x,y, button='left') that way it will drag the towers 
 
-
-time.sleep(1)
-pyautogui.click(966, 960)
-pyautogui.click(234, 98)
-pyautogui.click(944, 647)
-pyautogui.moveTo(472,963)
-pyautogui.mouseDown(button='left')
-pyautogui.moveTo(384, 635, 0.15)
-pyautogui.mouseUp(button='left')
 # print(pyautogui.size())
 screenSize = pyautogui.size()
 # print(screenSize[0], screenSize[1])
 
 if ((screenSize[0] == 1920) and (screenSize[1] == 1080)) :
     print("Correct")
+    time.sleep(1)
+    pyautogui.click(966, 960)
+    pyautogui.click(234, 98)
+    pyautogui.click(944, 647)
+    pyautogui.click(337, 95)
+    pyautogui.moveTo(472,963)
+    pyautogui.mouseDown(button='left')
+    pyautogui.moveTo(384, 635, 0.15)
+    pyautogui.mouseUp(button='left')
 else:
     print("Commit Sepuku") 
